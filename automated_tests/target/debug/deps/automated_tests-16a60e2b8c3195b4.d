30f459f846ffd3ff88fddb779dcf85fc3ed35d39
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-10-13 23:45:56 -0300

    add: An I/O Project: Building a Command Line Program module without tdd

--- automated_tests/target/debug/deps/automated_tests-16a60e2b8c3195b4.d
+++
@@ -1,5 +0,0 @@
-/home/jvdev/rust_doc/automated_tests/target/debug/deps/automated_tests-16a60e2b8c3195b4.rmeta: src/lib.rs
-
-/home/jvdev/rust_doc/automated_tests/target/debug/deps/automated_tests-16a60e2b8c3195b4.d: src/lib.rs
-
-src/lib.rs:
