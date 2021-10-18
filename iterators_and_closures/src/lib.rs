25ee7582d349227f1ea5902869a470f02229780a
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-10-17 23:17:56 -0300

    fix: processing a series of items with iterators

--- iterators_and_closures/src/lib.rs
+++ iterators_and_closures/src/lib.rs
@@ -51,7 +51,7 @@ mod tests{
     fn using_other_iterator_trait_methods(){
         let sum: u32 = Counter::new()
             .zip(Counter::new().skip(1))
-            .map(|a, b| a * b)
+            .map(|(a, b)| a * b)
             .filter(|x| x % 3 == 0)
             .sum();
         assert_eq!(18, sum);
