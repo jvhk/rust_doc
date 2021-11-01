fafe224774dd80a535f75640ebfe62f2abf4e407
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-10-31 21:38:39 -0300

    add: Shared-State Concurrency

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Arc,Mutex};
+use std::rc::Rc;
 
 
 fn main() {
@@ -36,6 +38,8 @@ fn main() {
     handle2.join().unwrap();
 
 */
+
+/*
 
     //Using Message Passing to Transfer Data Between Threads
     let (tx, rx) = mpsc::channel(); //sender / receiver
@@ -84,4 +88,36 @@ fn main() {
         println!("Got: {}", received);
     }
 
+*/
+
+    //16.3Shared-State Concurrency
+    //The API of Mutex<T>
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m); 
+
+
+    //Sharing a Mutex<T> Between Multiple Threads
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter =  Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
