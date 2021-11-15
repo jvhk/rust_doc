6cd72e2d1a8b853750e85c48f873b057243b2be0
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-11-14 23:41:05 -0300

    DEL: removing target/ folder

--- patern_matching/target/debug/deps/patern_matching-893cf7c16b9d8069.d
+++
@@ -1,5 +0,0 @@
-/home/hk/rust_things/rust_doc/patern_matching/target/debug/deps/patern_matching-893cf7c16b9d8069: src/main.rs
-
-/home/hk/rust_things/rust_doc/patern_matching/target/debug/deps/patern_matching-893cf7c16b9d8069.d: src/main.rs
-
-src/main.rs:
--- patern_matching/target/debug/patern_matching.d
+++
@@ -1,1 +0,0 @@
-/home/hk/rust_things/rust_doc/patern_matching/target/debug/patern_matching: /home/hk/rust_things/rust_doc/patern_matching/src/main.rs
